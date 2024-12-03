21a15c8699044421dda4f9bcac9ff19b4adc9bc0
tbxark <tbxark@outlook.com>
2024-12-03 23:33:36 +0800

    feat: 优化目录遍历逻辑，支持macOS 10.15及以上版本的文件枚举

--- Sources/rmtrash/rmtrash.swift
+++ Sources/rmtrash/rmtrash.swift
@@ -255,11 +255,24 @@ extension Trash {
         guard question("descend into directory: '\(path)'?") else {
             return
         }
-        let subs = (try? fileManager.contentsOfDirectory(atPath: path)) ?? []
-        for sub in subs {
-            let subPath = URL(fileURLWithPath: path).appendingPathComponent(sub).relativePath
-            removeOne(path: subPath)
+       
+        if #available(macOS 10.15, *), let enumerator = FileManager.default.enumerator(
+            at: URL(fileURLWithPath: path),
+            includingPropertiesForKeys: [],
+            options: [.skipsSubdirectoryDescendants, .producesRelativePathURLs]
+        )  {
+            for case let fileURL as URL in enumerator {
+                let subPath = URL(fileURLWithPath: path).appendingPathComponent(fileURL.relativePath).relativePath
+                removeOne(path: subPath)
+            }
+        } else {
+            let subs = (try? fileManager.contentsOfDirectory(atPath: path)) ?? []
+            for sub in subs {
+                let subPath = URL(fileURLWithPath: path).appendingPathComponent(sub).relativePath
+                removeOne(path: subPath)
+            }
         }
+        
         // try to remove the directory after all files in it are removed
         removeEmptyDirectory(path)
     }
